19fe03de364bfa7c2b706c6679aed6cf404589a7
MurchanaPatwary <161120629+MurchanaPatwary@users.noreply.github.com>
2026-05-06 23:26:24 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
-const clientUrl = process.env.CLIENT_URL || 'https://expensify-iota.vercel.app/';
+const clientUrl = 'https://expensify-iota.vercel.app';
 
 app.use(cors({ origin: clientUrl }));
 app.use(express.json());
